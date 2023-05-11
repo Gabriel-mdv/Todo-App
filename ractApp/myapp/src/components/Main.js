8381281f63b76b303d416ee6b74e35ac1556272e
Andela <andela@Andelas-MacBook-Pro.local>
2023-05-11 15:31:59 +0200

    initial commit

---
+++ ractApp/myapp/src/components/Main.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+
+
+function Main() {
+    return (
+        <main>
+            <div className='facts'>
+                <h1>Why Learn React</h1>
+
+                <ul>
+                    <li>It is declarative</li>
+                    <li>It allows leveraging reusable components</li>
+                    <li>It is hirable</li>
+                    <li>It is consistently maintained by smart people (facebook)</li>
+                </ul>
+            </div>
+        </main>
+    )
+}
+
+
+export default Main;
---
+++ ractApp/myapp/src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from "react";
+import logo from '../images/images.png'
+
+
+function Navbar(){
+    return(
+        <nav className="nav">
+            <div style={{display:'flex', alignItems: 'center', alignContent: 'flex-end'}}>
+            <img className="logo" src={logo} alt="react logo"/>
+
+           <h1 className="nav_title">We ❤️ React</h1>
+           </div>
+
+
+           <h2 className="project">project 0.0.1</h2>
+        </nav>
+    )
+}
+
+
+export default Navbar;
